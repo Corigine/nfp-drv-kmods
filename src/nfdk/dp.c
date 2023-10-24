Corigine/nfp-drv-kmods

commit a1c36074c43c1faef4e5e1d339a9bed8320eacb9

nfp: add nfdk tx metadata padding for alignment
Add metadata padding for the purposes of DMA alignment.

Ensure that the skb inline data is cacheline aligned by prepending zero
bytes. This has shown to significantly improve PCIe performance on some
systems.

Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfdk/dp.c
@@ -186,10 +186,12 @@ nfp_nfdk_prep_tx_meta(struct nfp_net_dp *dp, struct nfp_app *app,
 #else
 	void *md_dst = NULL;
 #endif
+	bool vlan_insert, pad_cap, pad_align = false;
 	struct nfp_ipsec_offload offload_info;
+	unsigned long cur_phys, target_phys;
 	unsigned char *data;
-	bool vlan_insert;
 	u32 meta_id = 0;
+	u8 pad_len = 0;
 	int md_bytes;
 
 #ifdef CONFIG_NFP_NET_IPSEC
@@ -203,8 +205,11 @@ nfp_nfdk_prep_tx_meta(struct nfp_net_dp *dp, struct nfp_app *app,
 #endif
 
 	vlan_insert = skb_vlan_tag_present(skb) && (dp->ctrl & NFP_NET_CFG_CTRL_TXVLAN_V2);
+	pad_cap = !!(dp->ctrl_w1 & NFP_NET_CFG_CTRL_META_PAD);
+	cur_phys = virt_to_phys(skb->data);
 
-	if (!(md_dst || vlan_insert || *ipsec))
+	if (!(md_dst || vlan_insert || *ipsec) &&
+	    !(pad_cap && (cur_phys % cache_line_size())))
 		return 0;
 
 	md_bytes = sizeof(meta_id) +
@@ -212,15 +217,39 @@ nfp_nfdk_prep_tx_meta(struct nfp_net_dp *dp, struct nfp_app *app,
 		   (vlan_insert ? NFP_NET_META_VLAN_SIZE : 0) +
 		   (*ipsec ? NFP_NET_META_IPSEC_FIELD_SIZE : 0);
 
+	/* Determine whether padding is necessary. */
+	cur_phys -= md_bytes;
+	if (pad_cap && (cur_phys % cache_line_size()))
+		pad_align = true;
+
+	/* Determine padding len such that skb->data is cache aligned. */
+	if (pad_align) {
+		md_bytes += NFP_NET_META_PAD_SIZE;
+		cur_phys -= NFP_NET_META_PAD_SIZE;
+		target_phys = ALIGN_DOWN(cur_phys, cache_line_size());
+		pad_len = cur_phys - target_phys;
+		md_bytes += pad_len;
+	}
+
 	if (unlikely(skb_cow_head(skb, md_bytes)))
 		return -ENOMEM;
 
 	data = skb_push(skb, md_bytes) + md_bytes;
+
+	if (pad_align) {
+		data -= pad_len;
+		memset(data, 0, pad_len);
+		data -= NFP_NET_META_PAD_SIZE;
+		*data = pad_len;
+		meta_id <<= NFP_NET_META_FIELD_SIZE;
+		meta_id |= NFP_NET_META_PAD;
+	}
 #ifdef COMPAT__HAVE_METADATA_IP_TUNNEL
 	if (md_dst) {
 		data -= NFP_NET_META_PORTID_SIZE;
 		put_unaligned_be32(md_dst->u.port_info.port_id, data);
-		meta_id = NFP_NET_META_PORTID;
+		meta_id <<= NFP_NET_META_FIELD_SIZE;
+		meta_id |= NFP_NET_META_PORTID;
 	}
 #endif
 	if (vlan_insert) {

src/nfp_net_ctrl.h
@@ -49,6 +49,7 @@
 #define NFP_NET_META_CONN_HANDLE	7
 #define NFP_NET_META_RESYNC_INFO	8 /* RX resync info request */
 #define NFP_NET_META_IPSEC		9 /* IPsec SA index for tx and rx */
+#define NFP_NET_META_PAD               10 /* Padding for dma alignment */
 
 #define NFP_META_PORT_ID_CTRL		~0U
 
@@ -58,6 +59,7 @@
 #define NFP_NET_META_CONN_HANDLE_SIZE		8
 #define NFP_NET_META_IPSEC_SIZE			4
 #define NFP_NET_META_IPSEC_FIELD_SIZE		12
+#define NFP_NET_META_PAD_SIZE			1
 /* Hash type pre-pended when a RSS hash was computed */
 #define NFP_NET_RSS_NONE		0
 #define NFP_NET_RSS_IPV4		1

commit c25cc84599aca5f7cedc4b93388fbba13c4055a1

nfp: avoid padding too long metadata in LSO case
Since only 8 bits are defined for header length in LSO descriptor,
and the header length includes prepended metadata if existing. And
the padding metadata in some environment whose cache line is 128B
aligned makes it possible that the final header length exceeds 255
bytes in some encapsulated and IPv6 cases. So add some code to drop
the padding metadata to avoid it happens.

Fixes: a1c36074c43c ("nfp: add nfdk tx metadata padding for alignment")
Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Reviewed-by: Louis Peens <louis.peens@corigine.com>
Reviewed-by: Ryno Swart <ryno.swart@corigine.com>

src/nfdk/dp.c
@@ -230,6 +230,27 @@ nfp_nfdk_prep_tx_meta(struct nfp_net_dp *dp, struct nfp_app *app,
 		target_phys = ALIGN_DOWN(cur_phys, cache_line_size());
 		pad_len = cur_phys - target_phys;
 		md_bytes += pad_len;
+
+		/* Check if we have sufficient space for padding, metadata + headers
+		 * must be less than 256 bytes due to LSO descriptor definiton.
+		 */
+		if (skb_is_gso(skb)) {
+			u32 hdrlen;
+
+			if (!skb->encapsulation)
+				hdrlen = skb_transport_offset(skb) + tcp_hdrlen(skb);
+			else
+				hdrlen = skb_inner_transport_header(skb) - skb->data +
+					 inner_tcp_hdrlen(skb);
+
+			if ((hdrlen + md_bytes) >> 8) {
+				pad_align = false;
+				md_bytes -= pad_len + NFP_NET_META_PAD_SIZE;
+				if (md_bytes == sizeof(meta_id))
+					/* no other metadata */
+					return 0;
+			}
+		}
 	}
 
 	if (unlikely(skb_cow_head(skb, md_bytes)))