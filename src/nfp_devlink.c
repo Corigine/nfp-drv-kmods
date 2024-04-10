c55f93b636025d1d3f399fa125e3fb8a334dcf4d
Fei Qin <fei.qin@corigine.com>
2024-04-10 13:26:36 +0200

    nfp: update devlink device info output
    Newer NIC will introduce a new part number, now add it
    into devlink device info.
    
    This patch also updates the information of "board.id" in
    nfp.rst to match the devlink-info.rst.
    
    Signed-off-by: Fei Qin <fei.qin@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [Fei: backport and add compat code]
    Signed-off-by: Fei Qin <fei.qin@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>

--- src/nfp_devlink.c
+++ src/nfp_devlink.c
@@ -251,7 +251,7 @@ static const struct nfp_devlink_versions_simple {
 	{ DEVLINK_INFO_VERSION_GENERIC_BOARD_REV,	"assembly.revision", },
 	{ DEVLINK_INFO_VERSION_GENERIC_BOARD_MANUFACTURE, "assembly.vendor", },
 	{ "board.model", /* code name */		"assembly.model", },
-	{ "board.pn",					"pn", },
+	{ DEVLINK_INFO_VERSION_GENERIC_BOARD_PART_NUMBER, "pn", },
 };
 
 static int
