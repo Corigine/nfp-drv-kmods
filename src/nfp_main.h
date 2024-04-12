Corigine/nfp-drv-kmods

commit 5573dc888ab71a37e51b5555af5979daabaa5324

nfp: add support for cpp cdev to use area cache
In our currently cpp implement, there are more and more reserved bars
in device probe stage. There are some cpp area caches which will be
used to keep the cpp area and bar when the cpp access is finished for
the following access to use. It is effective in most of the cases,
but in our current implement, the cpp access via cdev will not use
area cache which will lead the cpp access via cdev will not get the
bars hold by area cache.

When some user commands depend on some specified bars and the bars
are reserved or hold by area cache, the command will hang until the
require bar is released or replaced in cache.

So in this implement, we add support for cpp cdev to use area cache,
then the user command will not hang if the required bar is hold in
area cache.

Signed-off-by: Baowen Zheng <baowen.zheng@corigine.com>
Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_main.h
@@ -28,6 +28,7 @@ struct platform_device;
 
 struct nfp_cpp;
 struct nfp_cpp_area;
+struct nfp_cpp_area_cache;
 struct nfp_eth_table;
 struct nfp_hwinfo;
 struct nfp_mip;
@@ -283,4 +284,11 @@ unsigned int nfp_net_lr2speed(unsigned int linkrate);
 unsigned int nfp_net_speed2lr(unsigned int speed);
 
 u8 nfp_get_pf_id(struct nfp_pf *pf);
+struct nfp_cpp_area_cache *
+area_cache_get(struct nfp_cpp *cpp, u32 id,
+	       u64 addr, unsigned long *offset, size_t length);
+void
+area_cache_put(struct nfp_cpp *cpp, struct nfp_cpp_area_cache *cache);
+struct nfp_cpp_area *
+area_get_from_cache(struct nfp_cpp_area_cache *cache);
 #endif /* NFP_MAIN_H */

src/nfp_dev_cpp.c
@@ -394,6 +394,15 @@ static ssize_t nfp_dev_cpp_op(struct file *file,
 	}
 
 	while (count > 0) {
+		unsigned long long address = *offp;
+		struct nfp_cpp_area_cache *cache;
+		unsigned long offset = 0;
+
+		cache = area_cache_get(cpp, cpp_id, address, &offset, curlen);
+		if (cache) {
+			area = area_get_from_cache(cache);
+			goto data_access;
+		}
 		area = nfp_cpp_area_alloc_with_name(
 			cpp, cpp_id, "nfp.cdev", *offp, curlen);
 		if (!area)
@@ -413,6 +422,7 @@ static ssize_t nfp_dev_cpp_op(struct file *file,
 			return err;
 		}
 
+data_access:
 		for (pos = 0; pos < curlen; pos += len) {
 			len = curlen - pos;
 			if (len > sizeof(tmpbuf))
@@ -425,11 +435,11 @@ static ssize_t nfp_dev_cpp_op(struct file *file,
 					break;
 				}
 				err = nfp_cpp_area_write(
-					area, pos, tmpbuf, len);
+					area, pos + offset, tmpbuf, len);
 				if (err < 0)
 					break;
 			} else {
-				err = nfp_cpp_area_read(area, pos, tmpbuf, len);
+				err = nfp_cpp_area_read(area, pos + offset, tmpbuf, len);
 				if (err < 0)
 					break;
 				if (copy_to_user(udata, tmpbuf, len)) {
@@ -442,8 +452,10 @@ static ssize_t nfp_dev_cpp_op(struct file *file,
 		*offp += pos;
 		totlen += pos;
 		buff += pos;
-		nfp_cpp_area_release(area);
-		nfp_cpp_area_free(area);
+		if (cache)
+			area_cache_put(cpp, cache);
+		else
+			nfp_cpp_area_release_free(area);
 
 		if (err < 0)
 			return err;

src/nfpcore/nfp_cppcore.c
@@ -994,7 +994,13 @@ int nfp_cpp_area_cache_add(struct nfp_cpp *cpp, size_t size)
 	return 0;
 }
 
-static struct nfp_cpp_area_cache *
+struct nfp_cpp_area *
+area_get_from_cache(struct nfp_cpp_area_cache *cache)
+{
+	return cache->area;
+}
+
+struct nfp_cpp_area_cache *
 area_cache_get(struct nfp_cpp *cpp, u32 id,
 	       u64 addr, unsigned long *offset, size_t length)
 {
@@ -1076,7 +1082,7 @@ exit:
 	return cache;
 }
 
-static void
+void
 area_cache_put(struct nfp_cpp *cpp, struct nfp_cpp_area_cache *cache)
 {
 	if (!cache)

commit afc30299efe10cf759d8f9dbf69be4db533565a8

nfp: move area_cache function prototypes
Move function prototypes to appropriate header file to avoid
"no previous prototype" warnings.

Fixes: 5573dc888ab7 ("nfp: add support for cpp cdev to use area cache")
Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Reviewed-by: Baowen Zheng <baowen.zheng@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_main.h
@@ -284,11 +284,4 @@ unsigned int nfp_net_lr2speed(unsigned int linkrate);
 unsigned int nfp_net_speed2lr(unsigned int speed);
 
 u8 nfp_get_pf_id(struct nfp_pf *pf);
-struct nfp_cpp_area_cache *
-area_cache_get(struct nfp_cpp *cpp, u32 id,
-	       u64 addr, unsigned long *offset, size_t length);
-void
-area_cache_put(struct nfp_cpp *cpp, struct nfp_cpp_area_cache *cache);
-struct nfp_cpp_area *
-area_get_from_cache(struct nfp_cpp_area_cache *cache);
 #endif /* NFP_MAIN_H */

src/nfpcore/nfp_cpp.h
@@ -413,6 +413,13 @@ nfp_cpp_from_operations(const struct nfp_cpp_operations *ops,
 void *nfp_cpp_priv(struct nfp_cpp *priv);
 
 int nfp_cpp_area_cache_add(struct nfp_cpp *cpp, size_t size);
+struct nfp_cpp_area_cache *
+area_cache_get(struct nfp_cpp *cpp, u32 id,
+	       u64 addr, unsigned long *offset, size_t length);
+void
+area_cache_put(struct nfp_cpp *cpp, struct nfp_cpp_area_cache *cache);
+struct nfp_cpp_area *
+area_get_from_cache(struct nfp_cpp_area_cache *cache);
 
 /* The following section contains extensions to the
  * NFP CPP API, to be used in a Linux kernel-space context.