Corigine/nfp-drv-kmods

commit 6f6dc8448d692dc7a0988ede7a4e87af9e4f2b96

nfp: flower: register PF/VF representors with devlink
Register the PF and VF representor ports with devlink to allow
users to inspect device ports.

Example output:
$ devlink port show
pci/0000:01:00.0/0: type eth netdev enp1s0f0np0 flavour physical
	port 0 splittable false lanes 1
pci/0000:01:00.0/10: type eth netdev enp1s0f0np1 flavour physical
	port 1 splittable false lanes 1
pci/0000:01:00.0/48: type eth netdev eth3 flavour pcipf controller 0
	pfnum 0 external false splittable false
pci/0000:01:00.0/49: type eth netdev eth2 flavour pcivf controller 0
	pfnum 0 vfnum 0 external false splittable false
pci/0000:01:00.0/50: type eth netdev eth4 flavour pcivf controller 0
	pfnum 0 vfnum 1 external false splittable false

Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_devlink.c
@@ -13,6 +13,8 @@
 #include "nfp_main.h"
 #include "nfp_port.h"
 
+#define NFP_DEVLINK_ID_PF_START	NSP_ETH_MAX_COUNT
+
 static int
 nfp_devlink_fill_eth_port(struct nfp_port *port,
 			  struct nfp_eth_table_port *copy)
@@ -475,6 +477,7 @@ int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 	struct nfp_eth_table_port eth_port;
 	int __maybe_unused serial_len;
 	struct devlink *devlink;
+	int dl_port_index;
 	const u8 *serial;
 	int ret;
 
@@ -482,20 +485,44 @@ int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 	SET_NETDEV_DEVLINK_PORT(port->netdev, &port->dl_port);
 #endif
 
-	rtnl_lock();
-	ret = nfp_devlink_fill_eth_port(port, &eth_port);
-	rtnl_unlock();
-	if (ret)
-		return ret;
-
-	attrs.split = eth_port.is_split;
+	switch (port->type) {
+	case NFP_PORT_PHYS_PORT:
+		attrs.flavour = DEVLINK_PORT_FLAVOUR_PHYSICAL;
+		rtnl_lock();
+		ret = nfp_devlink_fill_eth_port(port, &eth_port);
+		rtnl_unlock();
+		if (ret)
+			return ret;
+		attrs.split = eth_port.is_split;
 #if VER_NON_RHEL_GE(5, 9) || VER_RHEL_GE(8, 4)
-	attrs.splittable = eth_port.port_lanes > 1 && !attrs.split;
-	attrs.lanes = eth_port.port_lanes;
+		attrs.splittable = eth_port.port_lanes > 1 && !attrs.split;
+		attrs.lanes = eth_port.port_lanes;
 #endif
-	attrs.flavour = DEVLINK_PORT_FLAVOUR_PHYSICAL;
-	attrs.phys.port_number = eth_port.label_port;
-	attrs.phys.split_subport_number = eth_port.label_subport;
+		attrs.phys.port_number = eth_port.label_port;
+		attrs.phys.split_subport_number = eth_port.label_subport;
+		dl_port_index = port->eth_id;
+		break;
+#if VER_NON_RHEL_GE(5, 3) || VER_RHEL_GE(8, 2)
+	case NFP_PORT_PF_PORT:
+		attrs.flavour = DEVLINK_PORT_FLAVOUR_PCI_PF;
+		attrs.pci_pf.pf = port->pf_id;
+		dl_port_index = NFP_DEVLINK_ID_PF_START + port->pf_id;
+		break;
+	case NFP_PORT_VF_PORT:
+		attrs.flavour = DEVLINK_PORT_FLAVOUR_PCI_VF;
+		attrs.pci_vf.pf = port->pf_id;
+		attrs.pci_vf.vf = port->vf_id;
+		dl_port_index = NFP_DEVLINK_ID_PF_START + 1 + port->vf_id;
+		break;
+#endif
+	default:
+		/* Unrecognised port types will not be registered with devlink,
+		 * but this is not considered a failure. Subsequent devlink
+		 * port access needs to be protected by
+		 * nfp_devlink_is_port_registered.
+		 */
+		return 0;
+	}
 	serial_len = nfp_cpp_serial(port->app->cpp, &serial);
 	memcpy(attrs.switch_id.id, serial, serial_len);
 	attrs.switch_id.id_len = serial_len;
@@ -505,9 +532,9 @@ int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 5, 0)
 	return devl_port_register_with_ops(devlink, &port->dl_port,
-					   port->eth_id, &nfp_devlink_port_ops);
+					   dl_port_index, &nfp_devlink_port_ops);
 #else
-	return devl_port_register(devlink, &port->dl_port, port->eth_id);
+	return devl_port_register(devlink, &port->dl_port, dl_port_index);
 #endif
 }
 

src/flower/main.c
@@ -465,6 +465,10 @@ nfp_flower_spawn_vnic_reprs(struct nfp_app *app,
 		eth_hw_addr_random(repr);
 		port_id = nfp_flower_cmsg_pcie_port(nfp_pcie, vnic_type, idx, queue);
 
+		err = nfp_devlink_port_register(app, port);
+		if (err)
+			goto err_reprs_clean;
+
 		err = nfp_repr_init(app, repr,
 				    port_id, port, priv->nn->dp.netdev);
 		if (err) {
@@ -473,6 +477,10 @@ nfp_flower_spawn_vnic_reprs(struct nfp_app *app,
 			nfp_repr_free(repr);
 			goto err_reprs_clean;
 		}
+
+#if (VER_NON_RHEL_LT(6, 2)) || (RHEL_RELEASE_LT(9, 305, 0, 0))
+		nfp_devlink_port_type_eth_set(port);
+#endif
 
 		RCU_INIT_POINTER(reprs->reprs[i], repr);
 		nfp_info(app->cpp, "%s%d Representor(%s) created\n",

src/nfp_net_main.c
@@ -200,7 +200,7 @@ err_debugfs_vnic_clean:
 	nfp_net_debugfs_dir_clean(&nn->debugfs_dir);
 	nfp_net_clean(nn);
 err_devlink_port_clean:
-	if (nn->port)
+	if (nfp_devlink_is_port_registered(nn->port))
 		nfp_devlink_port_unregister(nn->port);
 	return err;
 }
@@ -251,7 +251,7 @@ static void nfp_net_pf_clean_vnic(struct nfp_pf *pf, struct nfp_net *nn)
 #endif
 	nfp_net_debugfs_dir_clean(&nn->debugfs_dir);
 	nfp_net_clean(nn);
-	if (nn->port)
+	if (nfp_devlink_is_port_registered(nn->port))
 		nfp_devlink_port_unregister(nn->port);
 }
 

src/nfpcore/nfp_nsp.h
@@ -147,6 +147,9 @@ enum nfp_ethtool_link_mode_list {
 	NFP_MEDIA_LINK_MODES_NUMBER
 };
 
+#define NSP_ETH_NBI_PORT_COUNT		24
+#define NSP_ETH_MAX_COUNT		(2 * NSP_ETH_NBI_PORT_COUNT)
+
 #define NFP_FEC_AUTO		BIT(NFP_FEC_AUTO_BIT)
 #define NFP_FEC_BASER		BIT(NFP_FEC_BASER_BIT)
 #define NFP_FEC_REED_SOLOMON	BIT(NFP_FEC_REED_SOLOMON_BIT)

src/nfpcore/nfp_nsp_eth.c
@@ -19,8 +19,6 @@
 #include "nfp_nsp.h"
 #include "nfp6000/nfp6000.h"
 
-#define NSP_ETH_NBI_PORT_COUNT		24
-#define NSP_ETH_MAX_COUNT		(2 * NSP_ETH_NBI_PORT_COUNT)
 #define NSP_ETH_TABLE_SIZE		(NSP_ETH_MAX_COUNT *		\
 					 sizeof(union eth_table_entry))
 

commit d582f3b747171bfb9b3e6c9260efab0b069c7721

nfp: flower: fix VF generations failure on PF1
The dl_port_index calculation doesn't take multiple PF case into
consideration, which results in repetitive index when registering
devlink port and shows following error when generating VFs on PF1:

    # echo echo 4 > /sys/class/net/enp1s0f1np1/device/sriov_numvfs
    -bash: echo: write error: Device or resource busy

This fix patch removes pf_id into dl_port_index calculation and will not
affect PF0's registration.

Fixes: 6f6dc8448d69 ("nfp: flower: register PF/VF representors with devlink")
Signed-off-by: Tianyu Yuan <tianyu.yuan@corigine.com>
Reviewed-by: Ryno Swart <ryno.swart@corigine.com>
Reviewed-by: Baowen Zheng <baowen.zheng@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_devlink.c
@@ -508,7 +508,7 @@ int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 	case NFP_PORT_PF_PORT:
 		attrs.flavour = DEVLINK_PORT_FLAVOUR_PCI_PF;
 		attrs.pci_pf.pf = port->pf_id;
-		dl_port_index = NFP_DEVLINK_ID_PF_START + port->pf_id;
+		dl_port_index = NFP_DEVLINK_ID_PF_START;
 		break;
 	case NFP_PORT_VF_PORT:
 		attrs.flavour = DEVLINK_PORT_FLAVOUR_PCI_VF;