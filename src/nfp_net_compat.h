7cbe19cc17ebd1f3db05542ace64e4b4adadd2f2
Fei Qin <fei.qin@corigine.com>
2024-07-18 15:39:09 +0800

    nfp: using compat_flag for ethtool_coalesce and ethtool_ringparam
    Using compat_flag for the following APIs
    	get/set_coalesce
    	get/set_ringparam
    to avoid lengthy kernel version judgement.
    
    Signed-off-by: Fei Qin <fei.qin@corigine.com>
    Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>

--- src/nfp_net_compat.h
+++ src/nfp_net_compat.h
@@ -1658,17 +1658,11 @@ __printf(2, 3) void ethtool_sprintf(u8 **data, const char *fmt, ...);
 #define ethtool_puts(data, str) ethtool_sprintf((data), (str))
 #endif
 
-#if ! ((VER_NON_RHEL_OR_SLEL_LT(5, 17) && !COMPAT_OELINUX) || \
-       RHEL_RELEASE_LT(8, 408, 0, 0) || \
-       (RHEL_RELEASE_GE(9, 0, 0, 0) && RHEL_RELEASE_LT(9, 119, 0, 0)) || \
-       VER_OEL_LT(5, 10) || SLEL_LOCALVER_LT(5, 14, 21, 150500, 53))
+#ifdef COMPAT__ETHTOOL_RINGPARAM
 #define VERSION__ETHTOOL_RINGPARAM
 #endif
 
-#if (VER_NON_RHEL_OR_SLEL_GE(5, 15) || \
-     (RHEL_RELEASE_GE(8, 358, 0, 0) && RHEL_RELEASE_LT(9, 70, 0, 0)) || \
-     RHEL_RELEASE_GE(9, 119, 0, 0) || VER_OEL_GE(5, 10) || \
-     VER_SLEL_GE(5, 14, 21))
+#ifdef COMPAT__ETHTOOL_COALESCE
 #define VERSION__ETHTOOL_COALESCE
 #endif
 
