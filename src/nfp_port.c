Corigine/nfp-drv-kmods

commit 4f7abe1c0f0e6eb415338750fc3a9867285ab51e

nfp: flower: register phy representors with devlink
Register the physical representor ports with devlink to allow
users to inspect device ports.

Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_port.c
@@ -42,6 +42,11 @@ int nfp_port_get_port_parent_id(struct net_device *netdev,
 	port = nfp_port_from_netdev(netdev);
 	if (!port)
 		return -EOPNOTSUPP;
+	/* If devlink_port is registered, devlink core
+	 * is taking care of port parent ID.
+	 */
+	if (nfp_devlink_is_port_registered(port))
+		return -EOPNOTSUPP;
 
 	ppid->id_len = nfp_cpp_serial(port->app->cpp, &serial);
 	memcpy(&ppid->id, serial, ppid->id_len);
@@ -145,6 +150,11 @@ nfp_port_get_phys_port_name(struct net_device *netdev, char *name, size_t len)
 
 	port = nfp_port_from_netdev(netdev);
 	if (!port)
+		return -EOPNOTSUPP;
+	/* If devlink_port is registered, devlink core
+	 * is taking care of name formatting.
+	 */
+	if (nfp_devlink_is_port_registered(port))
 		return -EOPNOTSUPP;
 
 	switch (port->type) {

src/flower/main.c
@@ -581,6 +581,10 @@ nfp_flower_spawn_phy_reprs(struct nfp_app *app, struct nfp_flower_priv *priv)
 		SET_NETDEV_DEV(repr, &priv->nn->pdev->dev);
 		nfp_net_get_mac_addr(app->pf, repr, port);
 
+		err = nfp_devlink_port_register(app, port);
+		if (err)
+			goto err_reprs_clean;
+
 		cmsg_port_id = nfp_flower_cmsg_phys_port(phys_port);
 		err = nfp_repr_init(app, repr,
 				    cmsg_port_id, port, priv->nn->dp.netdev);
@@ -590,6 +594,10 @@ nfp_flower_spawn_phy_reprs(struct nfp_app *app, struct nfp_flower_priv *priv)
 			nfp_repr_free(repr);
 			goto err_reprs_clean;
 		}
+
+#if (VER_NON_RHEL_LT(6, 2)) || (RHEL_RELEASE_LT(9, 305, 0, 0))
+		nfp_devlink_port_type_eth_set(port);
+#endif
 
 		nfp_flower_cmsg_mac_repr_add(ctrl_skb, app->pf->multi_pf.en ? 0 : idx,
 					     eth_tbl->ports[idx].nbi,

src/nfp_devlink.c
@@ -457,6 +457,18 @@ static const struct devlink_port_ops nfp_devlink_port_ops = {
 };
 #endif
 
+bool nfp_devlink_is_port_registered(struct nfp_port *port)
+{
+	if (!port)
+		return false;
+
+#if VER_NON_RHEL_LT(6, 1) || VER_RHEL_LT(9, 2)
+	return !!port->dl_port.devlink;
+#else
+	return port->dl_port.registered;
+#endif
+}
+
 int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 {
 	struct compat__devlink_port_attrs attrs = {};

src/nfp_net_compat.c
@@ -112,6 +112,11 @@ int compat__flow_block_cb_setup_simple(struct tc_block_offload *f,
 #endif
 
 #if !COMPAT__HAS_DEVLINK
+bool nfp_devlink_is_port_registered(struct nfp_port *port)
+{
+	return false;
+}
+
 int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port)
 {
 	return 0;

src/nfp_net_compat.h
@@ -831,6 +831,11 @@ enum devlink_port_flavour {
 	DEVLINK_PORT_FLAVOUR_PHYSICAL,
 	DEVLINK_PORT_FLAVOUR_CPU,
 	DEVLINK_PORT_FLAVOUR_DSA,
+	DEVLINK_PORT_FLAVOUR_PCI_PF,
+	DEVLINK_PORT_FLAVOUR_PCI_VF,
+	DEVLINK_PORT_FLAVOUR_VIRTUAL,
+	DEVLINK_PORT_FLAVOUR_UNUSED,
+	DEVLINK_PORT_FLAVOUR_PCI_SF,
 };
 #endif
 

src/nfp_net_repr.c
@@ -310,6 +310,12 @@ nfp_repr_transfer_features(struct net_device *netdev, struct net_device *lower)
 static void nfp_repr_clean(struct nfp_repr *repr)
 {
 	unregister_netdev(repr->netdev);
+	if (nfp_devlink_is_port_registered(repr->port)) {
+#if (VER_NON_RHEL_LT(6, 2)) || (RHEL_RELEASE_LT(9, 305, 0, 0))
+		nfp_devlink_port_type_clear(repr->port);
+#endif
+		nfp_devlink_port_unregister(repr->port);
+	}
 	nfp_app_repr_clean(repr->app, repr->netdev);
 	dst_release((struct dst_entry *)repr->dst);
 	nfp_port_free(repr->port);

src/nfp_port.h
@@ -157,6 +157,7 @@ int nfp_net_refresh_eth_port(struct nfp_port *port);
 void nfp_net_refresh_port_table(struct nfp_port *port);
 int nfp_net_refresh_port_table_sync(struct nfp_pf *pf);
 
+bool nfp_devlink_is_port_registered(struct nfp_port *port);
 int nfp_devlink_port_register(struct nfp_app *app, struct nfp_port *port);
 void nfp_devlink_port_unregister(struct nfp_port *port);
 #if (VER_NON_RHEL_LT(6, 2)) || (RHEL_RELEASE_LT(9, 305, 0, 0))

commit e3a69e1009df1530b3d9a8db943e1f311faa6fec

nfp: compat: fix phys_port_name "Operation not supported" issue
Add compat to the `nfp_port_get_phys_port_name` netdev op so that the
port name is supplied on older kernels.

This fixes "Operation not supported" errors on kernels that have
devlink (>= v4.6) but don't yet have
`devlink_compat_phys_port_name_get` (< v5.2)

Fixes: 4f7abe1c0f0e ("nfp: flower: register phy representors with devlink")
Signed-off-by: Ryno Swart <ryno.swart@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/Kbuild
@@ -279,6 +279,9 @@ $(eval $(call add_compat_flag,$(srctree)/include/linux/ethtool.h, "struct kernel
 # Check for ethtool_ringparam
 $(eval $(call add_compat_flag,$(srctree)/include/linux/ethtool.h, "struct kernel_ethtool_ringparam \*",COMPAT__ETHTOOL_RINGPARAM))
 
+# Check for devlink_compat_phys_port_name_get
+$(eval $(call add_compat_flag,$(srctree)/include/net/devlink.h,devlink_compat_phys_port_name_get,COMPAT__HAVE_DEVLINK_PHYS_PORT_NAME))
+
 utsrelease_h_path :=$(srctree)/include/generated/utsrelease.h
 # Check for BC Linux
 $(eval $(call add_compat_flag,$(utsrelease_h_path),bclinux,COMPAT_BCLINUX=1))

src/nfp_port.c
@@ -152,11 +152,13 @@ nfp_port_get_phys_port_name(struct net_device *netdev, char *name, size_t len)
 	port = nfp_port_from_netdev(netdev);
 	if (!port)
 		return -EOPNOTSUPP;
+#ifdef COMPAT__HAVE_DEVLINK_PHYS_PORT_NAME
 	/* If devlink_port is registered, devlink core
 	 * is taking care of name formatting.
 	 */
 	if (nfp_devlink_is_port_registered(port))
 		return -EOPNOTSUPP;
+#endif
 
 	switch (port->type) {
 	case NFP_PORT_PHYS_PORT: