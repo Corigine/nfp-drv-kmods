Corigine/nfp-drv-kmods

commit 02f17f524fd9df177d786ea75b3a579901a056ea

nfp: add error recover handler to nfp driver
Add error recover handle to nfp driver, this includes callback of
error_detected, slot_reset and resume. Then there is chance for
hardware to recover from fatal error.

Signed-off-by: Baowen Zheng <baowen.zheng@corigine.com>
Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_main.c
@@ -1329,6 +1329,8 @@ static int nfp_pci_probe(struct pci_dev *pdev,
 		goto err_net_remove;
 	}
 
+	pci_save_state(pdev);
+
 	return 0;
 
 err_net_remove:
@@ -1494,11 +1496,99 @@ static void nfp_pci_error_reset_done(struct pci_dev *dev)
 }
 #endif
 
+/**
+ *  nfp_pci_error_detected - called when PCI error is detected
+ *  @pdev: Pointer to PCI device
+ *  @state: The current pci connection state
+ *
+ *  This function is called after a PCI bus error affecting
+ *  this device has been detected.
+ **/
+static pci_ers_result_t nfp_pci_error_detected(struct pci_dev *pdev,
+					       pci_channel_state_t state)
+{
+	struct nfp_pf *pf = pci_get_drvdata(pdev);
+
+	dev_warn(&pdev->dev, "nfp error detect reported.\n");
+	if (state == pci_channel_io_normal) {
+		dev_warn(&pdev->dev, "Non-correctable non-fatal error reported.\n");
+		return PCI_ERS_RESULT_CAN_RECOVER;
+	}
+
+	if (state == pci_channel_io_perm_failure)
+		return PCI_ERS_RESULT_DISCONNECT;
+
+	/* Only works when state is pci_channel_io_frozen */
+	if (pf) {
+		struct nfp_net *nn;
+
+		list_for_each_entry(nn, &pf->vnics, vnic_list)
+			nfp_net_recover(nn, false);
+	}
+	pci_disable_device(pdev);
+
+	/* Request a slot reset. */
+	return PCI_ERS_RESULT_NEED_RESET;
+}
+
+/**
+ *  nfp_pci_slot_reset - called after the pci bus has been reset.
+ *  @pdev: Pointer to PCI device
+ *
+ *  Restart the card from scratch, as if from a cold-boot. Implementation
+ *  resembles the first-half of the resume routine.
+ **/
+static pci_ers_result_t nfp_pci_slot_reset(struct pci_dev *pdev)
+{
+	pci_ers_result_t result;
+	int err = 0;
+
+	err = pci_enable_device(pdev);
+	if (err) {
+		dev_warn(&pdev->dev, "Can not enable pcie device.\n");
+		result = PCI_ERS_RESULT_DISCONNECT;
+		goto exit_err;
+	}
+	pci_set_master(pdev);
+	pci_restore_state(pdev);
+	pci_save_state(pdev);
+
+	result = PCI_ERS_RESULT_RECOVERED;
+
+exit_err:
+	return result;
+}
+
+/**
+ *  nfp_pci_resume - called when traffic can start flowing again.
+ *  @pdev: Pointer to PCI device
+ *
+ *  This callback is called when the error recovery driver tells us that
+ *  its OK to resume normal operation. Implementation resembles the
+ *  second-half of the resume routine.
+ */
+static void nfp_pci_resume(struct pci_dev *pdev)
+{
+	struct nfp_pf *pf = pci_get_drvdata(pdev);
+
+	if (pf) {
+		struct nfp_net *nn;
+
+		/* wait for reset to complete */
+		msleep(200);
+		list_for_each_entry(nn, &pf->vnics, vnic_list)
+			nfp_net_recover(nn, true);
+	}
+}
+
 static const struct pci_error_handlers nfp_pci_err_handler = {
 #if VER_NON_RHEL_GE(4, 13) || VER_RHEL_GE(8, 0)
         .reset_prepare = nfp_pci_error_reset_prepare,
         .reset_done = nfp_pci_error_reset_done,
 #endif
+	.error_detected = nfp_pci_error_detected,
+	.slot_reset = nfp_pci_slot_reset,
+	.resume = nfp_pci_resume
 };
 
 static struct pci_driver nfp_pci_driver = {

src/nfp_net.h
@@ -760,6 +760,7 @@ struct nfp_net {
 	struct nfp_app *app;
 
 	bool vnic_no_name;
+	bool err_recover;
 
 	struct nfp_port *port;
 
@@ -1065,6 +1066,8 @@ int nfp_net_ring_reconfig(struct nfp_net *nn, struct nfp_net_dp *new,
 
 int nfp_net_fs_add_hw(struct nfp_net *nn, struct nfp_fs_entry *entry);
 int nfp_net_fs_del_hw(struct nfp_net *nn, struct nfp_fs_entry *entry);
+
+int nfp_net_recover(struct nfp_net *nn, bool resume);
 
 #ifdef CONFIG_NFP_DEBUG
 void nfp_net_debugfs_create(void);

src/nfp_net_common.c
@@ -1205,15 +1205,28 @@ static int nfp_net_netdev_close(struct net_device *netdev)
 	 */
 	nfp_net_close_stack(nn);
 
-	/* Step 2: Tell NFP
-	 */
+	if (!nn->err_recover) {
+		/* Step 2: Tell NFP
+		*/
 #if LINUX_VERSION_CODE > KERNEL_VERSION(3, 15, 0)
-	if (nn->cap_w1 & NFP_NET_CFG_CTRL_MCAST_FILTER)
-		__dev_mc_unsync(netdev, nfp_net_mc_unsync);
+		if (nn->cap_w1 & NFP_NET_CFG_CTRL_MCAST_FILTER)
+			__dev_mc_unsync(netdev, nfp_net_mc_unsync);
 #endif
 
-	nfp_net_clear_config_and_disable(nn);
-	nfp_port_configure(netdev, false);
+		nfp_net_clear_config_and_disable(nn);
+		nfp_port_configure(netdev, false);
+	} else {
+		u32 r;
+		for (r = 0; r < nn->dp.num_rx_rings; r++) {
+			nfp_net_rx_ring_reset(&nn->dp.rx_rings[r]);
+			if (nfp_net_has_xsk_pool_slow(&nn->dp, nn->dp.rx_rings[r].idx))
+				nfp_net_xsk_rx_bufs_free(&nn->dp.rx_rings[r]);
+		}
+		for (r = 0; r < nn->dp.num_tx_rings; r++)
+			nfp_net_tx_ring_reset(&nn->dp, &nn->dp.tx_rings[r]);
+		for (r = 0; r < nn->dp.num_r_vecs; r++)
+			nfp_net_vec_clear_ring_data(nn, r);
+	}
 
 	/* Step 3: Free resources
 	 */
@@ -1413,14 +1426,15 @@ static int nfp_net_netdev_open(struct net_device *netdev)
 	err = nfp_net_open_alloc_all(nn);
 	if (err)
 		return err;
+	if (!nn->err_recover) {
+		err = netif_set_real_num_tx_queues(netdev, nn->dp.num_stack_tx_rings);
+		if (err)
+			goto err_free_all;
 
-	err = netif_set_real_num_tx_queues(netdev, nn->dp.num_stack_tx_rings);
-	if (err)
-		goto err_free_all;
-
-	err = netif_set_real_num_rx_queues(netdev, nn->dp.num_rx_rings);
-	if (err)
-		goto err_free_all;
+		err = netif_set_real_num_rx_queues(netdev, nn->dp.num_rx_rings);
+		if (err)
+			goto err_free_all;
+	}
 
 	/* Step 2: Configure the NFP
 	 * - Ifup the physical interface if it exists
@@ -3392,6 +3406,64 @@ static int nfp_net_read_caps(struct nfp_net *nn)
 		nn->cap &= nn->app->type->ctrl_cap_mask;
 
 	return 0;
+}
+
+static int nfp_net_reset(struct nfp_net *nn)
+{
+	int err;
+
+	/* Make sure the FW knows the netdev is supposed to be disabled here */
+	nn_writel(nn, NFP_NET_CFG_CTRL, 0);
+	nn_writeq(nn, NFP_NET_CFG_TXRS_ENABLE, 0);
+	nn_writeq(nn, NFP_NET_CFG_RXRS_ENABLE, 0);
+	nn_writel(nn, NFP_NET_CFG_CTRL_WORD1, 0);
+	err = nfp_net_reconfig(nn, NFP_NET_CFG_UPDATE_RING |
+				   NFP_NET_CFG_UPDATE_GEN);
+	if (err) {
+		nn_err(nn, "failed to reset nfp net\n");
+		return err;
+	}
+
+	return 0;
+}
+
+/**
+ * nfp_net_recover() - recover for the nfp_net structure
+ * @nn:		NFP Net device structure
+ * @resume:	If it is in resume process
+ * Return: 0 on success or negative errno on error.
+ */
+int nfp_net_recover(struct nfp_net *nn, bool resume)
+{
+	struct net_device *netdev;
+	int err = 0;
+
+	rtnl_lock();
+
+	if (!nn->dp.netdev) {
+		err = -EINVAL;
+		goto no_dev;
+	}
+
+	if (resume)
+		nfp_net_reset(nn);
+
+	netdev = nn->dp.netdev;
+	if (resume) {
+		if (netif_running(netdev))
+			netdev->netdev_ops->ndo_open(netdev);
+		netif_device_attach(netdev);
+		nn->err_recover = false;
+	} else {
+		nn->err_recover = true;
+		netif_device_detach(netdev);
+		if (netif_running(netdev))
+			netdev->netdev_ops->ndo_stop(netdev);
+	}
+
+no_dev:
+	rtnl_unlock();
+	return err;
 }
 
 /**

commit a38cc847674641e88efd5076c0fe31b06415a0d8

nfp: allow nfp to handle uncorrectable non-fatal error
In our previous implement, we will ignore uncorrectable non-fatal error
in error detect callback. But when uncorrectable non-fatal error is
reported, there will be irq request failure in the resume process as
following:
	__setup_irq.cold.55+0x26/0xcf
	request_threaded_irq+0x109/0x170
	? nfp_net_tx_timeout+0x30/0x30 [nfp]
	nfp_net_aux_irq_request.constprop.50+0x5c/0xf0 [nfp]
	nfp_net_open_alloc_all+0x33/0x1b0 [nfp]
	nfp_net_netdev_open+0x1b/0xb0 [nfp]
	nfp_net_recover+0xa7/0x120 [nfp]
	? pcie_portdrv_probe+0xe0/0xe0
So we need to handle uncorrectable non-fatal error to disable
irq for resume process to reuse.

Fixes: 02f17f524fd9 ("nfp: add error recover handler to nfp driver")
Signed-off-by: Baowen Zheng <baowen.zheng@corigine.com>
Reviewed-by: Yinjun Zhang <yinjun.zhang@corigine.com>
Signed-off-by: Louis Peens <louis.peens@corigine.com>

src/nfp_main.c
@@ -1510,15 +1510,10 @@ static pci_ers_result_t nfp_pci_error_detected(struct pci_dev *pdev,
 	struct nfp_pf *pf = pci_get_drvdata(pdev);
 
 	dev_warn(&pdev->dev, "nfp error detect reported.\n");
-	if (state == pci_channel_io_normal) {
-		dev_warn(&pdev->dev, "Non-correctable non-fatal error reported.\n");
-		return PCI_ERS_RESULT_CAN_RECOVER;
-	}
 
 	if (state == pci_channel_io_perm_failure)
 		return PCI_ERS_RESULT_DISCONNECT;
 
-	/* Only works when state is pci_channel_io_frozen */
 	if (pf) {
 		struct nfp_net *nn;
 

src/nfp_netvf_main.c
@@ -368,10 +368,6 @@ static pci_ers_result_t nfp_pci_vf_error_detected(struct pci_dev *pdev,
 	struct nfp_net *nn;
 
 	dev_warn(&pdev->dev, "nfp vf error detect reported.\n");
-	if (state == pci_channel_io_normal) {
-		dev_warn(&pdev->dev, "Non-correctable non-fatal error reported.\n");
-		return PCI_ERS_RESULT_CAN_RECOVER;
-	}
 
 	vf = pci_get_drvdata(pdev);
 	if (!vf || !vf->nn)