22ca63281022717b811c10c6339ae4c8c420b746
Weiwen Hu <weiwen.hu@corigine.com>
2024-10-23 10:40:02 +0800

    nfp: support the lldp rx configuration to save into hw-info
    This patch fixes three issues. The first one is to solidify the lldp-rx
    configuration to avoid loss during restart. The second one is to set
    the lldp tx switch name to be more user-friendly. The 'disable-fw-lldp'
    flag is being removed in favour of the more descriptive
    'enable-tx-lldp' flag, to match the recently added 'enable-rx-lldp'
    flag.
    
    Signed-off-by: Weiwen Hu <weiwen.hu@corigine.com>
    Reviewed-by: Louis Peens <louis.peens@corigine.com>
    Reviewed-by: Yong Xu <yong.xu@corigine.com>

--- src/nfp_net_debugfs.c
+++ src/nfp_net_debugfs.c
@@ -27,7 +27,7 @@ static int nfp_lldp_dcbx_show(struct seq_file *file, void *data)
 	struct nfp_dcb *dcb;
 	int i;
 
-	if (!nn || !nn->app_priv)
+	if (!nn->app_priv)
 		return 0;
 
 	remote_dcbcfg = &((struct nfp_app_nic_private *)nn->app_priv)->remote_dcbx;
